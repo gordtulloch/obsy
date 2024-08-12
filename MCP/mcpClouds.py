11671ec6c3909cd8a8498e4a976b054cd1601a5f
Gord Tulloch <gord.tulloch@gmail.com>
2024-08-12 17:36:51 -0500

    Update

--- MCP/mcpClouds.py
+++ MCP/mcpClouds.py
@@ -37,9 +37,9 @@ class McpClouds(object):
         # Set up the image paths if required
         if not os.path.exists(config.get("ALLSKYSAMPLEDIR")):
             os.makedirs(config.get("ALLSKYSAMPLEDIR"))
-        with CLASS_NAMES as className:
+        for className in self.CLASS_NAMES:
             if not os.path.exists(config.get("ALLSKYSAMPLEDIR")+"/"+className):
-            os.makedirs(config.get("ALLSKYSAMPLEDIR")+"/"+className)
+                os.makedirs(config.get("ALLSKYSAMPLEDIR")+"/"+className)
 
     def isCloudy(self,allSkyOutput=False,allskysampling=False):
         logger.info('Using keras model: %s', KERAS_MODEL)
