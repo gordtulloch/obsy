147eb71608975b3f3c7a2cf4e9540b2a8b3d348d
Gord Tulloch <gord.tulloch@gmail.com>
2025-01-19 12:52:50 -0600

    Revise mandatory fields

--- config/forms.py
+++ config/forms.py
@@ -5,13 +5,33 @@ class GeneralConfigForm(forms.ModelForm):
     class Meta:
         model = GeneralConfig
         fields = '__all__'
+        labels = {
+            'latitude': 'Default Latitude',
+            'longitude': 'Default Longitude',
+            'elevation': 'Default Elevation',
+        }
 
 class CommunicationsConfigForm(forms.ModelForm):
     class Meta:
         model = CommunicationsConfig
         fields = '__all__'
+        # hide the following fields
+        exclude = ['email_backend']
+        labels = {
+            'email_host': 'SMTP Server Host',
+            'email_port': 'SMTP Server Port',
+            'email_use_tls': 'Use TLS',
+            'email_host_user': 'SMTP Server Username',
+            'email_host_password': 'SMTP Server Password',
+            'sender_email': 'Sender Email Address',
+            'recipient_email': 'Recipient Email Address',
+        }
 
 class RepositoryConfigForm(forms.ModelForm):
     class Meta:
         model = RepositoryConfig
         fields = '__all__'
+        labels = {
+            'ppsourcepath': 'Path to load data from to insert into the Repository',
+            'pprepopath': 'Repository Path',
+        }
--- config/models.py
+++ config/models.py
@@ -7,16 +7,14 @@ class GeneralConfig(models.Model):
 
 class CommunicationsConfig(models.Model):
     email_backend = models.CharField(max_length=255)
-    email_host = models.CharField(max_length=255)
-    email_port = models.IntegerField()
-    email_use_tls = models.BooleanField()
-    email_host_user = models.CharField(max_length=255)
-    email_host_password = models.CharField(max_length=255)
-    sender_email = models.CharField(max_length=255)
-    recipient_email = models.CharField(max_length=255)
-    twilio_sid = models.CharField(max_length=255)
-    twilio_token = models.CharField(max_length=255)
+    email_host = models.CharField(max_length=255, blank=True, null=True)
+    email_port = models.IntegerField(blank=True, null=True)
+    email_use_tls = models.BooleanField(blank=True, null=True)
+    email_host_user = models.CharField(max_length=255, blank=True, null=True)
+    email_host_password = models.CharField(max_length=255, blank=True, null=True)
+    sender_email = models.CharField(max_length=255, blank=True, null=True)
+    recipient_email = models.CharField(max_length=255, blank=True, null=True)
 
 class RepositoryConfig(models.Model):
-    ppsourcepath = models.CharField(max_length=255)
-    pprepopath = models.CharField(max_length=255)
+    ppsourcepath = models.CharField(max_length=255, blank=True, null=True)
+    pprepopath = models.CharField(max_length=255, blank=True, null=True)
